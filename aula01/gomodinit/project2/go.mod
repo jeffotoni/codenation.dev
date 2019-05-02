ecec414e81342ee92ae7e1f17231354598fdea18
jefferson <jeff.otoni@gmail.com>
2019-05-02 14:29:44 -0300

    remove projeto2

--- aula01/gomodinit/project2/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/jeffotoni/codenation.dev/aula01/gomodinit/project2
-
-go 1.12
--- aula01/gomodinit/project2/pkg/math/math.go
+++
@@ -1,8 +0,0 @@
-package math
-
-import "fmt"
-
-func Sum(a, b int) {
-
-	fmt.Println("pacote math sum: ", a+b)
-}
