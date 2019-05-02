db8fad30cc8b352f077b10e826cc091484497f90
jefferson <jeff.otoni@gmail.com>
2019-05-02 14:29:19 -0300

    second commit

--- aula01/gomodinit/project2/go.mod
+++ aula01/gomodinit/project2/go.mod
@@ -1,3 +1,3 @@
-module project2
+module github.com/jeffotoni/codenation.dev/aula01/gomodinit/project2
 
 go 1.12
--- aula01/gomodinit/project2/main.go
+++ aula01/gomodinit/project2/main.go
@@ -1,8 +1,8 @@
 package main
 
 import "fmt"
-import "codenation.dev/aula01/gomodinit/project2/pkg/math"
-import "codenation.dev/aula01/gomodinit/project2/pkg/util"
+import "github.com/jeffotoni/codenation.dev/aula01/gomodinit/project2/pkg/math"
+import "github.com/jeffotoni/codenation.dev/aula01/gomodinit/project2/pkg/util"
 
 func main() {
 	fmt.Println("Projeto 2 github..")
