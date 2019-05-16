04b79c2d971556906465ebeb799695be90c12b69
jefferson <jeff.otoni@gmail.com>
2019-05-16 11:49:52 -0300

    documentando o programa pong e ping

--- modulo3/goroutine/go-chan-ping-pong.go
+++ modulo3/goroutine/go-chan-ping-pong.go
@@ -1,30 +1,33 @@
 // Ao usar canais como parâmetros de função, você pode
 // especifica se um canal serve apenas para enviar ou receber
-// valores. Esta especificidade aumenta a segurança do tipo
-// o programa.
+// valores.
 
 package main
 
-import "fmt"
-
-// unidirecional - somente write
-// send to receive-only type <-chan int
+// Esta função "ping" só aceita um canal para envio
+// valores. Seria um erro em tempo de compilação tentar
+// receber neste canal.
+// unidirecional - somente read
+// receive from send-only type chan<- string
 func ping(pings chan<- string, msg string) {
 	pings <- msg
 }
 
-// unidirecional - somente read
-// receive from send-only type chan<- int
+// Esta função "pong" aceita um canal para receber e um para enviar
+// unidirecional -  write e read
+// send to receive-only type <-chan string   // write
+// receive from send-only type chan<- string // read
 func pong(pings <-chan string, pongs chan<- string) {
 	msg := <-pings
 	pongs <- msg
 }
 
 func main() {
-	pings := make(chan string, 1)
-	pongs := make(chan string, 1)
+
+	pings := make(chan string, 1) // channel bi-direcional
+	pongs := make(chan string, 1) // channel bi-direcional
 
 	ping(pings, "Enviando mensagem @jeffotoni..")
 	pong(pings, pongs)
-	fmt.Println(<-pongs)
+	println(<-pongs)
 }
