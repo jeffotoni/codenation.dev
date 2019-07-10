819e6103cbc75836bdcb88a5279163903b69456f
jefferson <jeff.otoni@gmail.com>
2019-07-10 09:21:08 -0300

    news updates files

--- modulo1/hello/hello.go
+++ modulo1/hello/hello.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"time"
+)
 
 func main() {
 
-	fmt.Printf("\nvim-go")
+	var c1 = make(chan string)
+	go func() {
+		time.Sleep(time.Millisecond * 10)
+		c1 <- "Hello "
+	}()
+
+	go func() {
+		time.Sleep(time.Millisecond * 12)
+		c1 <- "World!"
+	}()
+
+	if 1 != 2 {
+		println("ok")
+	}
+
+	print(<-c1)
+	print(<-c1)
 }
--- modulo4/server-handler/static-files.go
+++ modulo4/server-handler/static-files.go
@@ -4,8 +4,10 @@ package main
 import "net/http"
 
 func main() {
+
 	// diretorio fisico
 	fs := http.FileServer(http.Dir("assets/"))
+
 	// mostra no browser localhost:8080/static
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":8080", nil)
